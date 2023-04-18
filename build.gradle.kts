184e34cba14e694810d5d5b2b4c80ac61d5d964f
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-19 00:27:45 +0900

    依存関係を追加した

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,12 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
 
+    // Ktor Plugins
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-swagger:$ktor_version")
+
     // Logger
     implementation("ch.qos.logback:logback-classic:$logback_version")
 
