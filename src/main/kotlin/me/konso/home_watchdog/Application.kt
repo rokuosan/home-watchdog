2b0f3004f59023c0ce9f952e2f25a5bfc233b7f5
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-19 00:02:45 +0900

    .envを読み取り機能を追加した

--- src/main/kotlin/me/konso/home_watchdog/Application.kt
+++ src/main/kotlin/me/konso/home_watchdog/Application.kt
@@ -3,12 +3,45 @@ package me.konso.home_watchdog
 import io.ktor.server.application.*
 import me.konso.home_watchdog.plugins.configureLogging
 import me.konso.home_watchdog.plugins.configureRouting
+import java.io.File
 
 fun main(args: Array<String>): Unit=
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    setEnvParam()
+
     configureLogging()
     configureRouting()
+}
+
+fun setEnvParam(){
+    val logger = Store.SystemLogger
+
+    logger.info("Reading .env file...")
+
+    val file = File(".env")
+    if(!file.exists()){
+        logger.info("Not found .env file on ${File("").absoluteFile}")
+        return
+    }
+
+    try{
+        file.bufferedReader().readLines().forEach { line ->
+            val record = line.split("=")
+            logger.debug("Read: $line")
+
+            if(record.size == 2){
+                val key = record[0].replace(" ", "")
+                val value = record[1]
+                System.setProperty(key, value)
+                logger.debug("Set: $key=$value")
+            }
+        }
+        logger.info("Complete reading .env file.")
+    }catch(e: Exception){
+        logger.debug(e.stackTraceToString())
+        logger.info("Failed to read .env file.")
+    }
 }
---
+++ src/main/kotlin/me/konso/home_watchdog/Store.kt
@@ -0,0 +1,8 @@
+package me.konso.home_watchdog
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+object Store {
+    val SystemLogger: Logger= LoggerFactory.getLogger("System")
+}
