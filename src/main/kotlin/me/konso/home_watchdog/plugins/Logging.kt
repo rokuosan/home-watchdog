8e88afec11783c4e260ce1cfea108b7d28e2b11c
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-18 23:12:27 +0900

    API呼び出し時にロギングするようにした

--- src/main/kotlin/me/konso/home_watchdog/plugins/Logging.kt
+++ src/main/kotlin/me/konso/home_watchdog/plugins/Logging.kt
@@ -1,2 +1,24 @@
 package me.konso.home_watchdog.plugins
 
+import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.request.*
+import org.slf4j.event.Level
+
+fun Application.configureLogging(){
+    install(CallLogging){
+        level = Level.INFO
+
+        filter { call ->
+            call.request.path().startsWith("/")
+        }
+
+        format { call ->
+            val status = call.response.status()
+            val method = call.request.httpMethod
+            val ua = call.request.headers["User-Agent"]
+            val path = call.request.path()
+            "${method.value} $status $path [$ua]"
+        }
+    }
+}
