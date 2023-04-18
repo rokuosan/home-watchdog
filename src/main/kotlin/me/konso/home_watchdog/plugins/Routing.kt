59f98091b34f710793152553943a625975514f64
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-19 01:35:38 +0900

    WIP | Webhook用エンドポイントを追加

--- src/main/kotlin/me/konso/home_watchdog/plugins/Routing.kt
+++ src/main/kotlin/me/konso/home_watchdog/plugins/Routing.kt
@@ -1,13 +1,24 @@
 package me.konso.home_watchdog.plugins
 
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import me.konso.home_watchdog.routes.api.lineRoute
 
 fun Application.configureRouting() {
+    install(ContentNegotiation){
+        json()
+    }
+
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Home Watchdog v0.1")
+        }
+
+        route("/api/"){
+            lineRoute()
         }
     }
 }
