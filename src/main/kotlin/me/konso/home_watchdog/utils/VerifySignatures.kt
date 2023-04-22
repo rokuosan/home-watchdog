558ce38b9083e9d90e7936e96908521e1ba8a6cd
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-22 22:42:57 +0900

    LINEからの署名の検証する関数を実装した

--- src/main/kotlin/me/konso/home_watchdog/utils/VerifySignatures.kt
+++ src/main/kotlin/me/konso/home_watchdog/utils/VerifySignatures.kt
@@ -1,22 +1,25 @@
 package me.konso.home_watchdog.utils
 
+import io.ktor.server.application.*
+import io.ktor.util.pipeline.*
 import java.util.*
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
-fun verifySignatures(signature: String, body: String): Boolean{
+fun PipelineContext<Unit, ApplicationCall>.verifySignatures(body: String): Boolean{
     return try{
         val secret = System.getProperty("CHANNEL_SECRET")
         val hmac = "HmacSHA256"
         val key = SecretKeySpec(secret.toByteArray(), hmac)
         val mac = Mac.getInstance(hmac)
+        val sig = this.call.request.headers["x-line-signature"]?:return false
 
         mac.init(key)
 
         val src = body.toByteArray(Charsets.UTF_8)
         val result = Base64.getEncoder().encodeToString(mac.doFinal(src))
 
-        signature == result
+        sig == result
     }catch(e: Exception){
         false
     }
