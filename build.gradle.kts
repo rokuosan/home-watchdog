2002de50b451e9203c73c8cc6168ee584ddf41ac
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-23 00:47:00 +0900

    依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val line_version: String by project
+val okhttp_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.20"
@@ -32,6 +34,13 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-swagger:$ktor_version")
+
+    // LINE
+    implementation("com.linecorp.bot:line-bot-model:$line_version")
+    implementation("com.linecorp.bot:line-bot-parser:$line_version")
+    implementation("com.linecorp.bot:line-bot-api-client:$line_version")
+    implementation("com.squareup.okhttp3:okhttp:$okhttp_version")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okhttp_version")
 
     // Logger
     implementation("ch.qos.logback:logback-classic:$logback_version")
