49bcd47ee236a8ba61d76cf503ca90fc6ef148c5
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-23 00:49:41 +0900

    LINEクライアントを共通化した

--- src/main/kotlin/me/konso/home_watchdog/Store.kt
+++ src/main/kotlin/me/konso/home_watchdog/Store.kt
@@ -1,8 +1,10 @@
 package me.konso.home_watchdog
 
+import com.linecorp.bot.client.LineMessagingClient
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object Store {
     val SystemLogger: Logger= LoggerFactory.getLogger("System")
+    lateinit var LINEBotClient: LineMessagingClient
 }
