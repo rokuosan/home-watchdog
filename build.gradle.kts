e644b598a718f6c04ac3db6d87454c6f2aa682cc
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-23 21:32:59 +0900

    依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 val line_version: String by project
 val okhttp_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.20"
@@ -34,6 +35,14 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cors-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-swagger:$ktor_version")
+
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
+    // Postgres
+    implementation("org.postgresql:postgresql:42.2.27")
 
     // LINE
     implementation("com.linecorp.bot:line-bot-model:$line_version")
