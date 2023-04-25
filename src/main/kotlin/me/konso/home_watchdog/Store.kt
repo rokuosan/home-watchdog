452fd4fb61bd81fb46ab1f9e88e0aaf49211525a
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-26 00:07:10 +0900

    DBコンフィグの初期値を設定した

--- src/main/kotlin/me/konso/home_watchdog/Store.kt
+++ src/main/kotlin/me/konso/home_watchdog/Store.kt
@@ -2,6 +2,7 @@ package me.konso.home_watchdog
 
 import com.linecorp.bot.client.LineMessagingClient
 import me.konso.home_watchdog.database.dao.DaoFacade
+import me.konso.home_watchdog.entities.DatabaseConfig
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -9,4 +10,13 @@ object Store {
     val SystemLogger: Logger= LoggerFactory.getLogger("System")
     lateinit var LINEBotClient: LineMessagingClient
     lateinit var dao: DaoFacade
+
+    object Defaults{
+        val DATABASE_CONFIG = DatabaseConfig(
+            databaseDriver = "org.postgresql.Driver",
+            databaseUrl = "jdbc:postgresql://localhost:5432/postgres",
+            databaseUser = "postgres",
+            databasePass = "postgres"
+        )
+    }
 }
