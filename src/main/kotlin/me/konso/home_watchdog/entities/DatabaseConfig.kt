6abd3c945e2e3994911eb1285bccd9621ab3d517
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-26 00:06:47 +0900

    DB接続情報を保存するJSONのエンティティを追加

---
+++ src/main/kotlin/me/konso/home_watchdog/entities/DatabaseConfig.kt
@@ -0,0 +1,42 @@
+package me.konso.home_watchdog.entities
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+/**
+ * データベースの接続に利用される設定ファイル。
+ */
+@Serializable
+data class DatabaseConfig(
+    /**
+     * データベース接続に用いるユーザー名。
+     */
+    @SerialName("database_user")
+    val databaseUser: String,
+
+
+    /**
+     * データベース接続に用いるパスワード。
+     * ここで利用するパスワードはSHA256でハッシュしたものを記述してください。
+     */
+    @SerialName("database_pass")
+    val databasePass: String,
+
+
+    /**
+     * データベースに接続するためのドライバー。
+     * 現在はPostgresのみ対応しています。
+     */
+    @SerialName("database_driver")
+    val databaseDriver: String,
+
+    /**
+     * データベースに接続するためのURL。
+     * データベース名を含めたURLで記述する必要があります。
+     */
+    @SerialName("database_url")
+    val databaseUrl: String
+
+
+)
