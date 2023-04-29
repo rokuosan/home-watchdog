99be5cbe1bf689a7b481a5979e51b6c449e88e39
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-04-29 21:36:40 +0900

    LineEventクラスからイベントタイプによって処理を分けるようにした

--- src/main/kotlin/me/konso/home_watchdog/entities/LineEvent.kt
+++ src/main/kotlin/me/konso/home_watchdog/entities/LineEvent.kt
@@ -1,6 +1,7 @@
 package me.konso.home_watchdog.entities
 
 import kotlinx.serialization.Serializable
+import me.konso.home_watchdog.events.execute
 
 @Serializable
 data class LineEvent(
@@ -12,4 +13,6 @@ data class LineEvent(
     val replyToken: String? = null,
     val source: LineSource? = null,
     val message: LineMessage? = null
-)
+){
+    suspend fun executeEvent() = this.execute()
+}
