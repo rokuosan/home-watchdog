2cfe4763aced082d9ba06d4b6510ce4db58b6bec
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-05-04 22:09:31 +0900

    テーブルに列を追加

--- src/main/kotlin/me/konso/home_watchdog/database/models/Users.kt
+++ src/main/kotlin/me/konso/home_watchdog/database/models/Users.kt
@@ -4,14 +4,16 @@ import org.jetbrains.exposed.sql.Table
 
 data class User(
     val id: String,
-    val isAuthorized: Boolean,
-    val permissionLevel: Int,
+    val isAuthorized: Boolean = false,
+    val permissionLevel: Int = 0,
+    val notificationLevel: Int = 0
 )
 
 object Users: Table(){
     val id = varchar("id", 64)
     val isAuthorized = bool("is_authorized").default(false)
     val permissionLevel = integer("permission_level").default(0)
+    val notificationLevel = integer("notification_level").default(0)
 
     override val primaryKey = PrimaryKey(id)
 }
