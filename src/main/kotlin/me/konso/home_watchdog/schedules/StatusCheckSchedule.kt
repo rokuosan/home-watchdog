d0bab3715cf051dfb6a6bb3cb90ef4971f03f542
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-05-04 22:04:00 +0900

    死活管理対象サーバの状態変化に伴う通知を送信するようにした

--- src/main/kotlin/me/konso/home_watchdog/schedules/StatusCheckSchedule.kt
+++ src/main/kotlin/me/konso/home_watchdog/schedules/StatusCheckSchedule.kt
@@ -1,5 +1,7 @@
 package me.konso.home_watchdog.schedules
 
+import com.linecorp.bot.model.PushMessage
+import com.linecorp.bot.model.message.TextMessage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -36,17 +38,55 @@ class StatusCheckSchedule: Schedule {
             val config = Json.decodeFromString<TargetsConfig>(str)
             this.interval = config.interval
 
+            for (server in config.servers){
+                Store.ServerStatus[server] = false
+            }
+
             this.scheduler = Scheduler(kotlinx.coroutines.Runnable {
                 for (server in config.servers){
                     CoroutineScope(Dispatchers.IO).launch {
                         val p = Ping(server, config.timeout)
-                        logger.debug("HOST: $server => ${p.ping()}")
+                        val prev = Store.ServerStatus[server]?:false
+                        val res = p.ping()
+                        logger.debug("HOST: $server => $res")
+
+                        Store.ServerStatus[server] = res
+
+                        if(prev && !res){
+                            // Previous is true(= Server is up)
+                            // But now is false(= Server is down)
+                            sendConnectionAlert(server, true)
+                        }
+                        if(!prev && res){
+                            sendConnectionAlert(server, false)
+                        }
                     }
                 }
             })
 
             this.every = Every(config.interval, TimeUnit.MILLISECONDS)
 
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+    }
+}
+
+private suspend fun sendConnectionAlert(server: String, goneDown: Boolean){
+    val msg = if(goneDown){
+        "🔴ALERT\nHost(${server}) has been down."
+    }else{
+        "🟢ALERT\nHost(${server}) has been up."
+    }
+
+    val users = Store.dao.getAllUsers()
+
+    for(user in users){
+        val push = PushMessage(user.id, TextMessage(msg))
+
+        try{
+            val res = Store.LINEBotClient.pushMessage(push).get()
+            println("Alert was sent to ${user.id} [${res.requestId}]")
         }catch (e: Exception){
             e.printStackTrace()
         }
