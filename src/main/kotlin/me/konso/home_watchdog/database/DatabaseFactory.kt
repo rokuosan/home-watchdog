5b5dc20808be99651dd5644cc93313035cb7e0b0
rokuosan <85651386+rokuosan@users.noreply.github.com>
2023-05-04 17:21:48 +0900

    出力される文字列を修正

--- src/main/kotlin/me/konso/home_watchdog/database/DatabaseFactory.kt
+++ src/main/kotlin/me/konso/home_watchdog/database/DatabaseFactory.kt
@@ -29,18 +29,18 @@ object DatabaseFactory{
 
             // ファイルの存在を確認する。存在しない場合はデフォルトを出力
             if(!file.exists()){
-                logger.debug("database.json がありませんでした。")
+                logger.debug("database.json does not exists.")
                 val default = Store.Defaults.DATABASE_CONFIG
                 val json = jsonFormat.encodeToString(default)
 
                 logger.debug(json)
-                logger.debug("database.jsonを作成します。")
+                logger.debug("Create database.json")
                 file.bufferedWriter(Charsets.UTF_8).use {
                     it.append(json)
                     it.newLine()
                 }
             }else {
-                logger.debug("database.json を発見しました。")
+                logger.debug("database.json was found!")
             }
 
             // ファイル読み取り
@@ -68,10 +68,10 @@ object DatabaseFactory{
             }
 
         }catch (e : Exception){
-            logger.info("DB接続に失敗しました。接続情報を確認してください。")
+            logger.info("Connection to database failed. Please check the connection information.")
             try{
                 CoroutineScope(context = Dispatchers.IO).launch {
-                    logger.debug("サーバー停止処理を開始します")
+                    logger.debug("Stopping the server...")
                     delay(1000)
                     val host = "localhost"
                     var port = "8080"
@@ -80,7 +80,7 @@ object DatabaseFactory{
                         port = this.toString()
                     }
                     val url = "http://$host:$port/${shutdown.replaceFirst("/", "")}"
-                    logger.debug("ShutdownURL($url)にアクセスします")
+                    logger.debug("Connect to ShutdownURL($url).")
                     val con = URL(url).openConnection() as HttpURLConnection
                     con.readTimeout = 5_000
                     con.connectTimeout = 5_000
